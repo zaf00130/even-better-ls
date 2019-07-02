00bd252f4a6da404432b5d6b60c18110c33a0d52
Fredrik Edvardsson <fredrik.edv@gmail.com>
2019-07-02 20:20:06 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -11,14 +11,13 @@ sudo cp ls_colors_generator.py /usr/bin/ls_colors_generator
 # Cleanup
 [ -d "coreutils-${COREUTILS_VER}" ] && \
     rm -rf coreutils-${COREUTILS_VER}
-[ -f "coreutils-${COREUTILS_VER}.tar.xz" ]  && \
-    rm -f coreutils-${COREUTILS_VER}.tar.xz
 
 #
 # Download coreutils and patch
-wget https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VER}.tar.xz
+[ ! -f "coreutils-${COREUTILS_VER}.tar.xz" ]  && \
+    wget https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VER}.tar.xz
+
 tar -xf coreutils-${COREUTILS_VER}.tar.xz
-rm coreutils-${COREUTILS_VER}.tar.xz
 cd coreutils-${COREUTILS_VER}
 patch -p0 < ../ls.patch
 
@@ -30,4 +29,3 @@ for bin in ls dir vdir; do
     echo "Copying ${bin} to /usr/bin/${bin}-i"
     sudo cp src/${bin} /usr/bin/${bin}-i
 done
-
